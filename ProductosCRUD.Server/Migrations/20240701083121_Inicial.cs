442c20ba53f52ad135e56d495e936f76b8de474b
Pablo A <pablo.alberca8@gmail.com>
2024-07-13 21:51:39 -0500

    Commit inicial proyecto

---
+++ ProductosCRUD.Server/Migrations/20240701083121_Inicial.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductosCRUD.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    idActividad = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombreActividad = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    conocimientoActividad = table.Column<decimal>(type: "decimal(4,2)", nullable: false),
+                    relacionamientoActividad = table.Column<decimal>(type: "decimal(4,2)", nullable: false),
+                    condicionTrabajoActividad = table.Column<decimal>(type: "decimal(4,2)", nullable: false),
+                    impactoActividad = table.Column<decimal>(type: "decimal(4,2)", nullable: false),
+                    responsabilidadActividad = table.Column<decimal>(type: "decimal(4,2)", nullable: false),
+                    ponderacionActividad = table.Column<decimal>(type: "decimal(4,2)", nullable: false),
+                    ponderacionNivelActividad = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    grupoOcupacionalActividad = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    observacionActividad = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.idActividad);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_nombreActividad",
+                table: "Productos",
+                column: "nombreActividad",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
---
+++ ProductosCRUD.Server/Models/Producto.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductosCRUD.Server.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int idActividad { get; set; }
+
+        [MaxLength(256, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Display(Name = "Nombre de la Actividad...............")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string nombreActividad { get; set; } = null!;
+
+        [Column(TypeName = "decimal(4,2)")]
+        [Display(Name = "Conocimiento")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal conocimientoActividad { get; set; }
+
+        [Column(TypeName = "decimal(4,2)")]
+        [Display(Name = "Relacionamiento")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal relacionamientoActividad { get; set; }
+
+        [Column(TypeName = "decimal(4,2)")]
+        [Display(Name = "Condicion Trabajo")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal condicionTrabajoActividad { get; set; }
+
+        [Column(TypeName = "decimal(4,2)")]
+        [Display(Name = "Impacto")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal impactoActividad { get; set; }
+
+        [Column(TypeName = "decimal(4,2)")]
+        [Display(Name = "Responsabilidad")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal responsabilidadActividad { get; set; }
+
+        [Column(TypeName = "decimal(4,2)")]
+        [Display(Name = "Ponderacion")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal ponderacionActividad { get; set; }
+
+        [MaxLength(32, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Display(Name = "Ponderacion Nivel")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string ponderacionNivelActividad { get; set; } = null!;
+
+        [MaxLength(32, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Display(Name = "Grupo Ocupacional")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string grupoOcupacionalActividad { get; set; } = null!;
+
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Observacion Actividad")]
+        [MaxLength(256, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string? observacionActividad { get; set; }
+
+
+    }
+}
